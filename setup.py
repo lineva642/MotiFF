3f0e1bca20d1eb0944617759ac429794179875e6
lineva642 <96028@mail.ru>
2020-11-19 01:08:27 +0300

    fixed setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name                 = 'pyMotif',
+    name                 = 'MotiFF',
     version              = '0.0.1',
-    #TODO: написать description
     description          = '''An utility for recognition of consensus motifs and other amino acid enrichments in large scale proteomics''',
-    long_description     = (''.join(open('README.MD').readlines())),
+    long_description     = (''.join(open('README.md').readlines())),
+    long_description_content_type = 'text/markdown',
     author               = 'Victoria Lineva & Julia Bubis',
     author_email         = 'lineva.vi@phystech.edu',
     install_requires     = ['pyteomics>=4.2', 'pandas','argparse','logging', 'os', 'collections', 're', 'scipy', 'numpy'],
@@ -17,5 +17,5 @@ setup(
                             'Topic :: Scientific/Engineering :: Physics'],
     license              = 'License :: OSI Approved :: Apache Software License',
     packages             = find_packages(),
-    entry_points         = {'console_scripts': ['pyMotif=pyMotif.main:main']}
+    entry_points         = {'console_scripts': ['MotiFF=MotiFF.main:main']}
     )
